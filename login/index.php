6a0bccd35899ab5013e62c40ae5103c3369fe882
Alex Maguire <alm62@aber.ac.uk>
2014-12-01 22:27:42 +0000

    Update index.php

--- login/index.php
+++ login/index.php
@@ -8,3 +8,55 @@
         > Client side, check for empty fields
         > Server side, check that the login id exists (is valid) and check the password is correct
 -->
+
+<html>
+    <head>
+        <title>Group Projext</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+            </head>
+    <body>
+        <header>
+            <h1>Plant Database</h1>
+        </header>
+        <nav>
+            <ul id="navigation">
+                <li>
+                    <a href="index.html">Home</a>
+                </li>
+                <li>
+                    <a href="records.html">Recordings</a>
+                </li>
+                <li>
+                    <a href="reserves.html">Reserves</a>
+                </li>
+                <li>
+                    <a href="login.html">
+                        <span style="color:white">Login</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <section>
+            <form>
+                
+                Login ID:
+                
+                <p>
+                <input type="text" name="login">
+                    </p>
+                    <br>
+                    Password:
+                    
+                    <p>
+                    <input type="password" name="password">
+                        </p>
+                        <br>
+                        <input type="submit" value="Submit">
+                            </form>
+        </section>
+        <footer>
+            2014 - Group Project - Group 10
+            
+        </footer>
+    </body>
+</html>
