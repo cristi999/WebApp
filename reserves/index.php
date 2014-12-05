7685681abf567d60b1da7de88743f799d47ce4c0
PeterNewbold <peter.newbold179@gmail.com>
2014-12-05 14:17:01 +0000

    Updated Database Column Names

--- reserves/index.php
+++ reserves/index.php
@@ -1,6 +1,6 @@
 <!-- VIEW RESERVES PAGE [reserves/index.php]
    - Displays a table containing a list of all the reserves and information about them
-        | id | reserve name | location | description |
+        | rsv_id | rsv_name | rsv_loc | rsv_desc |
    - Reserves will probably be displayed in alphabetical order [A-Z]
    - Must include an option to add (create) a new reserve
    - Must include options to update or delete a reserve (probably at the end of each table row)
